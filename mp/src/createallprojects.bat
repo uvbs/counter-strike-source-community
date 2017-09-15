c349ed22e21c04409de3ad540285f0b9ab6b8da5
Mr Nick <nikodash2020@hotmail.com>
2017-09-15 12:20:14 -0500

    Initial Commit #3

---
+++ mp/src/createallprojects.bat
@@ -0,0 +1,3 @@
+@echo off
+devtools\bin\vpc.exe /cstrike /2013 +everything /mksln Everything_SDK.sln
+pause
---
+++ mp/src/creategameprojects.bat
@@ -0,0 +1,3 @@
+@echo off
+devtools\bin\vpc.exe /cstrike /2013 +game /mksln Game-CSTRIKE.sln
+pause
